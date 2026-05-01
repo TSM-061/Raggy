66b28295440bd696e0b656d0a7dde05ba49a21d9
Thomas M <tsmoxham@gmail.com>
2026-05-02 00:42:38 +1000

    chore(database): upgrade pgx driver to v5

--- services/simple-auth-service/internal/session/postgres_repo.go
+++ services/simple-auth-service/internal/session/postgres_repo.go
@@ -7,8 +7,8 @@ import (
 
 	"github.com/TSM-061/Raggy/shared/serviceerr"
 	"github.com/google/uuid"
-	"github.com/jackc/pgx/v4"
-	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type PostgresSessionRepo struct {
--- services/simple-auth-service/internal/user/postgres_repo.go
+++ services/simple-auth-service/internal/user/postgres_repo.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 
 	"github.com/TSM-061/Raggy/shared/serviceerr"
-	"github.com/jackc/pgx/v4"
-	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type PostgresRepo struct {
