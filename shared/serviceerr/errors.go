f2ef76c020ecc7092f0353daeb14808ce5fabfc8
Thomas M <tsmoxham@gmail.com>
2026-05-05 01:30:15 +1000

    feat(shared): add additional service errors

--- shared/serviceerr/errors.go
+++ shared/serviceerr/errors.go
@@ -3,6 +3,9 @@ package serviceerr
 import "errors"
 
 var (
+	Internal     = errors.New("internal error")
+	InvalidInput = errors.New("invalid input")
 	NotFound     = errors.New("resource not found")
 	Unauthorized = errors.New("unauthorized")
+	Conflict     = errors.New("resource already exists")
 )
