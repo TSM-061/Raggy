7dceaf2d1c5ecc3c6815539c9b9a0caf74d263da
Thomas M <tsmoxham@gmail.com>
2026-05-30 20:52:06 +1000

    feat(logging): add context attached logger instance

---
+++ shared/logger/logger.go
@@ -0,0 +1,22 @@
+package logger
+
+import (
+	"context"
+	"log/slog"
+)
+
+type contextKey struct{}
+
+var loggerCtxKey contextKey = contextKey{}
+
+func ToContext(ctx context.Context, logger *slog.Logger) context.Context {
+	return context.WithValue(ctx, loggerCtxKey, logger)
+}
+
+func FromContext(ctx context.Context) *slog.Logger {
+	if logger, ok := ctx.Value(loggerCtxKey).(*slog.Logger); ok {
+		return logger
+	}
+
+	return slog.Default()
+}
